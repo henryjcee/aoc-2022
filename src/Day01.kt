b8880b827df59fa7305e88e91ce4f442d71785db
henryjcee <henry.course@gmail.com>
2022-12-02 15:42:39 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.joinToString("\n")
+            .split("\n\n")
+            .map { it.split("\n") }
+            .map { it.fold(0) { acc, i -> acc + i.toInt() } }
+            .max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.joinToString("\n")
+            .split("\n\n")
+            .map { it.split("\n") }
+            .map { it.fold(0) { acc, i -> acc + i.toInt() } }
+            .sorted()
+            .takeLast(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
